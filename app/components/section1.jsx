02d1672147e41ddd50e99ccae4f3acd93791ba5f
mo <mo@example.com>
2026-04-13 15:18:24 +0100

    update

--- app/components/section1.jsx
+++ app/components/section1.jsx
@@ -11,11 +11,76 @@ export default function Section1() {
     <div
       data-aos="fade-up"
       data-aos-anchor-placement="top-bottom"
-      className="w-full h-[70vh]  flex justify-center items-center"
-    >
-      <div className="h-11/12 w-3/12  border-l border-l-gray-300 border-r border-r-white  hover:border-r-gray-300  transition duration-400"></div>
-      <div className="h-11/12 w-3/12 border-l border-r border-l-white border-r-white hover:border-r-gray-300 hover:border-l-gray-300 transition duration-400 "></div>
-      <div className="h-11/12 w-3/12 border-r border-r-gray-300  border-l border-l-white  hover:border-l-gray-300  transition duration-400"></div>
+      className="w-full h-[40vw]  flex justify-center items-center"
+    > <Link href="/signpage" className="h-11/12 w-3/12  border-l border-l-gray-300 border-r border-r-white hover:border-r-gray-300 transition-all duration-500 flex flex-col items-center justify-around group">
+      
+        <div className="w-10/12 h-8/12  flex items-center justify-center transform transition-all duration-500 group-hover:scale-x-110 group-hover:scale-y-110">
+          <div className="w-full    flex items-center justify-center transform transition-all duration-500 group-hover:scale-x-115 group-hover:scale-y-110">
+            <img src="setup1234.png" alt="" />
+          </div>
+        </div>
+        <div className="w-10/12 h-2/12 sm:text-sm md:text-md lg:text-xl  text-center font-aldrich flex flex-col items-center justify-center  transform transition-all duration-500 group-hover:scale-x-120 group-hover:scale-y-110">
+          <p >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+            veniam!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Cupiditate, asperiores!
+          </p>
+        </div>
+      
+      </Link>
+      <Link href="/signpage" className="h-11/12 w-3/12 border-l border-l-white border-r border-r-white hover:border-r-gray-300 hover:border-l-gray-300 transition-all duration-500 flex flex-col items-center justify-around group">
+      
+        <div className="w-10/12 h-8/12  transform transition-all duration-500 group-hover:scale-x-120 group-hover:scale-y-115 flex items-center justify-center">
+          <div className=" relative flex items-center justify-center w-full max-w-125">
+            <img src="/gpu1.png" className="w-5/12 " alt="" />
+            <img
+              src="gpumr1.png"
+              className="absolute top-[40.8%] left-[40%] w-[23%] animate-[spin_3s_linear_infinite] group-hover:animate-spin"
+              alt=""
+            />
+            <img
+              src="gpumr1.png"
+              className="absolute top-[13.5%] left-[39.8%] w-[23%] animate-[spin_3s_linear_infinite] group-hover:animate-spin"
+              alt=""
+            />
+            <img
+              src="gpumr1.png"
+              className="absolute top-[67.9%] left-[39.9%] w-[23%] animate-[spin_3s_linear_infinite] group-hover:animate-spin"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="w-10/12 h-2/12 sm:text-sm md:text-md lg:text-xl  text-center font-aldrich flex flex-col items-center justify-center transform transition-all duration-500 group-hover:scale-x-120 group-hover:scale-y-110">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+            veniam!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Cupiditate, asperiores!
+          </p>
+        </div>
+      
+      </Link>
+      <Link href="/signpage" className="h-11/12 w-3/12   border-l border-l-white border-r border-r-gray-300 hover:border-l-gray-300 transition-all duration-500 flex flex-col items-center justify-around group">
+      
+        <div className="w-10/12 h-8/12  flex items-center justify-center transform transition-all duration-500 group-hover:scale-x-115 group-hover:scale-y-110">
+          <img src="lap1234.png" alt="" />
+        </div>
+        <div className="w-10/12 h-2/12 sm:text-sm md:text-md lg:text-xl  text-center font-aldrich flex flex-col items-center justify-center transform  transition-all duration-500 group-hover:scale-x-120 group-hover:scale-y-110">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+            veniam!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Cupiditate, asperiores!
+          </p>
+        </div>
+      </Link>
     </div>
   );
 }
