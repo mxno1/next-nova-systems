541b245b67e15aa236d392fe22b47c79e1bde934
mo <mo@example.com>
2026-04-13 19:12:00 +0100

    update

--- app/components/section1.jsx
+++ app/components/section1.jsx
@@ -77,7 +77,7 @@ export default function Section1() {
         </div>
         <div className="w-10/12 h-2/12 sm:text-sm md:text-md lg:text-xl  text-center font-aldrich flex flex-col items-center justify-center transform  transition-all duration-500 group-hover:scale-x-120 group-hover:scale-y-110">
           <p>
-            Lorem ipsum dolor sit amet conseic Lorem ipsum dolor, sit amet
+            Lorem ipsum dolor sit amet conseic Loremakzjbdceh ipsum dolor, sit amet
             consectetur adipisicing elit. Expedita, iusto! veniam!
           </p>
           <p>Lorem ipsum dolor sit</p>
