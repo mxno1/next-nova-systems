2851b8b98483d50621731a8b1b16bdec6872e893
mo <mo@example.com>
2026-04-13 15:58:30 +0100

    update

--- app/components/section1.jsx
+++ app/components/section1.jsx
@@ -12,15 +12,19 @@ export default function Section1() {
       data-aos="fade-up"
       data-aos-anchor-placement="top-bottom"
       className="w-full h-[40vw]  flex justify-center items-center"
-    > <Link href="/signpage" className="h-11/12 w-3/12  border-l border-l-gray-300 border-r border-r-white hover:border-r-gray-300 transition-all duration-500 flex flex-col items-center justify-around group">
-      
+    >
+      {" "}
+      <Link
+        href="/signpage"
+        className="h-11/12 w-3/12  border-l border-l-gray-300 border-r border-r-white hover:border-r-gray-300 transition-all duration-500 flex flex-col items-center justify-around group"
+      >
         <div className="w-10/12 h-8/12  flex items-center justify-center transform transition-all duration-500 group-hover:scale-x-110 group-hover:scale-y-110">
           <div className="w-full    flex items-center justify-center transform transition-all duration-500 group-hover:scale-x-115 group-hover:scale-y-110">
             <img src="setup1234.png" alt="" />
           </div>
         </div>
         <div className="w-10/12 h-2/12 sm:text-sm md:text-md lg:text-xl  text-center font-aldrich flex flex-col items-center justify-center  transform transition-all duration-500 group-hover:scale-x-120 group-hover:scale-y-110">
-          <p >
+          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
             veniam!
           </p>
@@ -29,10 +33,11 @@ export default function Section1() {
             Cupiditate, asperiores!
           </p>
         </div>
-      
       </Link>
-      <Link href="/signpage" className="h-11/12 w-3/12 border-l border-l-white border-r border-r-white hover:border-r-gray-300 hover:border-l-gray-300 transition-all duration-500 flex flex-col items-center justify-around group">
-      
+      <Link
+        href="/signpage"
+        className="h-11/12 w-3/12 border-l border-l-white border-r border-r-white hover:border-r-gray-300 hover:border-l-gray-300 transition-all duration-500 flex flex-col items-center justify-around group"
+      >
         <div className="w-10/12 h-8/12  transform transition-all duration-500 group-hover:scale-x-120 group-hover:scale-y-115 flex items-center justify-center">
           <div className=" relative flex items-center justify-center w-full max-w-125">
             <img src="/gpu1.png" className="w-5/12 " alt="" />
@@ -63,22 +68,20 @@ export default function Section1() {
             Cupiditate, asperiores!
           </p>
         </div>
-      
       </Link>
-      <Link href="/signpage" className="h-11/12 w-3/12   border-l border-l-white border-r border-r-gray-300 hover:border-l-gray-300 transition-all duration-500 flex flex-col items-center justify-around group">
-      
+      <Link
+        href="/signpage"
+        className="h-11/12 w-3/12   border-l border-l-white border-r border-r-gray-300 hover:border-l-gray-300 transition-all duration-500 flex flex-col items-center justify-around group"
+      >
         <div className="w-10/12 h-8/12  flex items-center justify-center transform transition-all duration-500 group-hover:scale-x-115 group-hover:scale-y-110">
           <img src="lap1234.png" alt="" />
         </div>
         <div className="w-10/12 h-2/12 sm:text-sm md:text-md lg:text-xl  text-center font-aldrich flex flex-col items-center justify-center transform  transition-all duration-500 group-hover:scale-x-120 group-hover:scale-y-110">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+            Lorem ipsum dolor sit amet consectetur adipisic
             veniam!
           </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Cupiditate, asperiores!
-          </p>
+          <p>Lorem ipsum dolor sit</p>
         </div>
       </Link>
     </div>
